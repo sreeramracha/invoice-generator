36f25eb14e742d2fa28a970929701d369c20eb31
Sreeram Racha <rachasreeram@gmail.com>
2024-11-16 18:24:30 +0530

    Added Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+	return (
+		<>
+			<div className="header">
+				<p>M/S. TEJASWINI SALES CORPORATION</p>
+				<p>
+					MIG-59/5, 3rd Phase, Near Ramya Grounds, Beside Vara Siddi
+					Vinaya Temple, K.P.H.B. Colony, Kukatpally, Hyderabad -
+					500072. <strong>Cell: 9849280926</strong> E-Mail:
+					tejaswinisalecorporation@gmail.com
+				</p>
+				<p>
+					<strong>GSTIN : 36BGUPR2488A1ZY</strong>
+				</p>
+			</div>
+		</>
+	);
+}
